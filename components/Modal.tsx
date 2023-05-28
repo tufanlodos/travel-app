4323b66ad8b3a012fd408768f7c595844f0f7bcf
mert <mertcelilozdin@hotmail.com>
2023-05-28 22:05:32 +0300

    modal animation added

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,7 +7,7 @@ export function Modal({ children }: Props) {
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none fade-in-box">
             {children}
           </div>
         </div>
